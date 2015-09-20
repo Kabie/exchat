2028a2bddda43c7a79766b23c502379b14b77080
Kabie <kabie3000@gmail.com>
2015-09-20 20:05:32 +0800

    fix prod

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :exchat, Exchat.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: "kabie.me", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
@@ -58,4 +58,7 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
+
+config :exchat, Exchat.Repo,
+  adapter: Ecto.Adapters.Postgres
